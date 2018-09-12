0ad8308f771f8a0d28a6ec6d47fcd48788c29dd1
thainguyenvann <thaitancang001@gmail.com>
2018-09-12 12:17:18 +0700

    finish while_loop

--- src/Thai/dev/Basic.java
+++ src/Thai/dev/Basic.java
@@ -3,14 +3,13 @@ package Thai.dev;
 public class Basic {
 	
 		
-		public static void main(String[] args) {
-	        int a = 0;
-	        for (int i = 1; i <= 100; i++) { 
-	            a = a + i;
+	public static void main(String[] args) {
+		
+	      int i = 1;
+	        while(i <= 5050 ){
+	        	i= i+i ;
+	            System.out.println("TONG = " +i);
+	            i++;
 	        }
-	        System.out.println("TONG CUA 100 SO DAU TIEN = " + a);
 	    }
-	}
-	        
-	        
-	
+}
